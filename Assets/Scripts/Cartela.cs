9f6c438fd16a6ffb61e1f765b2bb3ff0cf44b7f1
mariana mano <40200338@esmad.ipp.pt>
2021-06-09 22:13:12 +0100

    Mariana Ribeiro
    Cartela;
    Luz no PortalMain;
    Otimização labirinto;
    Otimização luzes "Final_1";
    
    09-06

---
+++ Assets/Scripts/Cartela.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cartela : MonoBehaviour
+{
+    [SerializeField]
+    float tempoCartela = 0f;
+    private bool esperaTempo = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        esperaTempo = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (esperaTempo)
+        {
+            tempoCartela = tempoCartela + Time.deltaTime;
+            if (tempoCartela >= 7f)
+            {
+                esperaTempo = false;
+                SceneManager.LoadScene("Menu Inicial");
+
+            }
+
+        }
+    }
+}
