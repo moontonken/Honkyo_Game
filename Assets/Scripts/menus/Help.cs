1306036236bcce4f3a54bcce381cb5246abf9314
mariana mano <40200338@esmad.ipp.pt>
2021-06-10 14:10:44 +0100

    Mariana Ribeiro
    Otimização "Menu Inicial"
    Otimização "Help"
    Otimização "Créditos"
    
    10-06

---
+++ Assets/Scripts/menus/Help.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Help : MonoBehaviour
+{
+   
+    void Update()
+    {
+        Loadscene();
+    }
+
+    void Loadscene()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene("Help");
+        }
+    }
+
+}
